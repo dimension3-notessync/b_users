12a40ee8dd0fc8b11ac56d130761324cfd512534
hszgLorKor <lorenz.korber@stud.hszg.de>
2026-04-03 14:58:07 +0200

    reworked call handling

--- routes/list/GET.js
+++ routes/list/GET.js
@@ -7,21 +7,21 @@ export default async function listHandler(req, res, tokenPort, usersDB) {
         return res.status(400).send({ message: "Token is missing" });
     }
 
-    axios.post(`http://${tokenPort}/permissionLevel`, {
-        token: token,
-        allowedRoles: ["admin"]
-    }, {
-        stepName : "adminCheck"
-    })
-        .then((adminCheckResponse) => {
-            return axios.get(`http://${usersDB}/all`, {
-                stepName : "requestAllUsers"
-            })
-        })
-        .then((usersDBresponse) => {
-            return res.status(200).send({users : usersDBresponse.data});
-        })
-        .catch(error => {
-            return errorHandler(req, res, error)
+    try {
+        const adminCheckResponse = await axios.post(`http://${tokenPort}/permissionLevel`, {
+            token: token,
+            allowedRoles: ["admin"]
+        }, {
+            stepName: "adminCheck"
         });
+
+        const usersDBresponse = await axios.get(`http://${usersDB}/all`, {
+            stepName: "requestAllUsers"
+        });
+
+        return res.status(200).send({ users: usersDBresponse.data.data });
+
+    } catch (error) {
+        return errorHandler(req, res, error);
+    }
 }
