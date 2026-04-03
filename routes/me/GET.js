f60d9173d0ad19b967e7c9f6ff72d3d40516d807
hszgLorKor <lorenz.korber@stud.hszg.de>
2026-04-03 14:52:10 +0200

    reworked call handling

--- routes/me/GET.js
+++ routes/me/GET.js
@@ -8,28 +8,28 @@ export default async function profileHandler(req, res, usersDB, tokenPort, subsc
     }
     let username;
     let email;
+    let permissionLevel;
 
-    axios.get(`http://${tokenPort}/user/${token}`, {
-        stepName : "requestUsernameByToken"
-    })
-        .then((usernameFromTokenResponse) => {
-            username = usernameFromTokenResponse.data.username;
-            return axios.post(`http://${usersDB}/user`, {
-                username: username
-            }, {
-                stepName : "requestUserInformation"
-            })
-        })
-        .then((usersDBresponse) => {
-            email = usersDBresponse.data.email;
-            return axios.get(`http://${subscriptionsDB}/user/${usersDBresponse.data.id}`, {
-                stepName : "requestSubscriptionsOfUser"
-            })
-        })
-        .then((subscriptionsDBresponse) => {
-            return res.status(200).send({ username: username, email: email, subscriptions: subscriptionsDBresponse.data});
-        })
-        .catch(error => {
-            return errorHandler(req, res, error);
+    try {
+        const usernameFromTokenResponse = await axios.get(`http://${tokenPort}/user/${token}`, {
+            stepName : "requestUsernameByToken"
         });
+        username = usernameFromTokenResponse.data.username;
+
+        const usersDBResponse = await axios.post(`http://${usersDB}/user`, {
+            username: username
+        }, {
+            stepName: "requestUserInformation"
+        })
+        email = usersDBresponse.data.email;
+
+        const subscriptionsDBresponse = axios.get(`http://${subscriptionsDB}/user/${usersDBresponse.data.id}`, {
+            stepName : "requestSubscriptionsOfUser"
+        })
+        permissionLevel = usersDBResponse.data.userInformation.permissionLevel;
+
+        return res.status(200).send({ username: username, email: email, permissionLevel, subscriptions: subscriptionsDBresponse.data});
+    } catch(error) {
+        return errorHandler(req, res, error);
+    }
 }
