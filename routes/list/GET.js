8b23e051f4c528003b561a970c773c9968747812
hszgLorKor <lorenz.korber@stud.hszg.de>
2026-04-03 14:51:42 +0200

    added missing parameters

--- routes/list/GET.js
+++ routes/list/GET.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import errorHandler from "../../utils/errorHandler.js";
 
-export default async function listHandler(req, res) {
+export default async function listHandler(req, res, tokenPort, usersDB) {
     const token = req.cookies.token;
     if (!token) {
         return res.status(400).send({ message: "Token is missing" });
@@ -19,7 +19,7 @@ export default async function listHandler(req, res) {
             })
         })
         .then((usersDBresponse) => {
-            return res.status(200).send({users: usersDBresponse.data});
+            return res.status(200).send({users : usersDBresponse.data});
         })
         .catch(error => {
             return errorHandler(req, res, error)
