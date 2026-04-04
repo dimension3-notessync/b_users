3ab2be18bed82353e5534708d477d65849f70daa
hszgLorKor <lorenz.korber@stud.hszg.de>
2026-04-04 10:37:26 +0200

    added rate limiter to endpoint

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 
 import express from 'express';
+import { rateLimit } from 'express-rate-limit'
 import cookieParser from 'cookie-parser'
+
 import createHandler from "./routes/POST.js";
 import deleteHandler from "./routes/DELETE.js";
 import profileHandler from "./routes/me/GET.js";
@@ -30,9 +32,18 @@ if (tokenPort === "undefined") {
     process.exit(1);
 }
 
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+    standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+    ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
+})
+
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+app.use(limiter);
 const router = express.Router();
 
 
